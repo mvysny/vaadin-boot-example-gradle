7d166be6e125ec6c47f59202d94922297c0f6061
Martin Vysny <mavi@vaadin.com>
2022-10-26 11:25:24 +0300

    now uses vaadin boot

--- src/main/java/com/vaadin/starter/skeleton/Main.java
+++ src/main/java/com/vaadin/starter/skeleton/Main.java
@@ -1,122 +1,14 @@
 package com.vaadin.starter.skeleton;
 
-import com.vaadin.flow.server.VaadinServlet;
-import com.vaadin.flow.server.startup.ServletContextListeners;
-import org.eclipse.jetty.server.Server;
-import org.eclipse.jetty.util.resource.Resource;
-import org.eclipse.jetty.webapp.*;
+import com.github.mvysny.vaadinboot.VaadinBoot;
 import org.jetbrains.annotations.NotNull;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
-import java.net.MalformedURLException;
-import java.net.URL;
 
 /**
  * Run {@link #main(String[])} to launch your app in Embedded Jetty.
  * @author mavi
  */
 public final class Main {
-
-    // mark volatile: might be accessed by the shutdown hook from a different thread.
-    private volatile static Server server;
-
     public static void main(@NotNull String[] args) throws Exception {
-        start(args);
-
-        // We want to shut down the app cleanly by calling stop().
-        // Unfortunately, that's not easy. When running from:
-        // * Intellij as a Java app: CTRL+C doesn't work but Enter does.
-        // * ./gradlew run: Enter doesn't work (no stdin); CTRL+C kills the app forcibly.
-        // * ./mvnw exec:java: both CTRL+C and Enter works properly.
-        // * cmdline as an unzipped app (production): both CTRL+C and Enter works properly.
-        // Therefore, we'll use a combination of the two.
-
-        // this gets called both when CTRL+C is pressed, and when main() terminates.
-        Runtime.getRuntime().addShutdownHook(new Thread(() -> stop("Shutdown hook called, shutting down")));
-        System.out.println("Press ENTER or CTRL+C to shutdown");
-        // Await for Enter.  ./gradlew run offers no stdin and read() will return immediately with -1
-        if (System.in.read() == -1) {
-            // running from Gradle
-            System.out.println("Running from Gradle, press CTRL+C to shutdown");
-            server.join(); // blocks endlessly
-        } else {
-            stop("Main: Shutting down");
-        }
+        new VaadinBoot().withArgs(args).run();
     }
-
-    public static void start(@NotNull String[] args) throws Exception {
-        // change this to e.g. /foo to host your app on a different context root
-        final String contextRoot = "/";
-
-        // detect&enable production mode
-        if (isProductionMode()) {
-            // fixes https://github.com/mvysny/vaadin14-embedded-jetty/issues/1
-            System.out.println("Production mode detected, enforcing");
-            System.setProperty("vaadin.productionMode", "true");
-        }
-
-        final WebAppContext context = new WebAppContext();
-        context.setBaseResource(findWebRoot());
-        context.setContextPath(contextRoot);
-        context.addServlet(VaadinServlet.class, "/*");
-        context.setAttribute("org.eclipse.jetty.server.webapp.ContainerIncludeJarPattern", ".*\\.jar|.*/classes/.*");
-        context.setConfigurationDiscovered(true);
-        context.getServletContext().setExtendedListenerTypes(true);
-        context.addEventListener(new ServletContextListeners());
-
-        int port = 8080;
-        if (args.length >= 1) {
-            port = Integer.parseInt(args[0]);
-        }
-        server = new Server(port);
-        server.setHandler(context);
-        server.start();
-
-        System.out.println("\n\n=================================================\n" +
-        "Please open http://localhost:" + port + contextRoot + " in your browser\n" +
-        "If you see the 'Unable to determine mode of operation' exception, just kill me and run `./gradlew vaadinPrepareFrontend`\n" +
-        "=================================================\n");
-    }
-
-    public static void stop(String reason) {
-        try {
-            if (server != null) {
-                log.info(reason);
-                server.stop(); // blocks until the webapp stops fully
-                log.info("Stopped");
-                server = null;
-            }
-        } catch (Throwable t) {
-            log.error("stop() failed: " + t, t);
-        }
-    }
-
-    private static boolean isProductionMode() {
-        final String probe = "META-INF/maven/com.vaadin/flow-server-production-mode/pom.xml";
-        final ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
-        return classLoader.getResource(probe) != null;
-    }
-
-    @NotNull
-    private static Resource findWebRoot() throws MalformedURLException {
-        // don't look up directory as a resource, it's unreliable: https://github.com/eclipse/jetty.project/issues/4173#issuecomment-539769734
-        // instead we'll look up the /webapp/ROOT and retrieve the parent folder from that.
-        final URL f = Main.class.getResource("/webapp/ROOT");
-        if (f == null) {
-            throw new IllegalStateException("Invalid state: the resource /webapp/ROOT doesn't exist, has webapp been packaged in as a resource?");
-        }
-        final String url = f.toString();
-        if (!url.endsWith("/ROOT")) {
-            throw new RuntimeException("Parameter url: invalid value " + url + ": doesn't end with /ROOT");
-        }
-        log.info("/webapp/ROOT is " + f);
-
-        // Resolve file to directory
-        URL webRoot = new URL(url.substring(0, url.length() - 5));
-        log.info("WebRoot is " + webRoot);
-        return Resource.newResource(webRoot);
-    }
-
-    private static final Logger log = LoggerFactory.getLogger(Main.class);
 }
