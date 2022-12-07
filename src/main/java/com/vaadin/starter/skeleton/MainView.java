f772d1a70c71e43dba303f182a871a0c91ae8730
Martin Vysny <mavi@vaadin.com>
2022-12-07 09:15:32 +0200

    minor

--- src/main/java/com/vaadin/starter/skeleton/MainView.java
+++ src/main/java/com/vaadin/starter/skeleton/MainView.java
@@ -7,7 +7,6 @@ import com.vaadin.flow.component.notification.Notification;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.component.textfield.TextField;
 import com.vaadin.flow.router.Route;
-import com.vaadin.starter.skeleton.services.GreetService;
 import com.vaadin.starter.skeleton.services.Services;
 
 /**
