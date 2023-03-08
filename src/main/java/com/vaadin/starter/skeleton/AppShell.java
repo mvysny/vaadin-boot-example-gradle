b17de6b72917ed95eaace09527d1dee4de9fad0c
Martin Vysny <mavi@vaadin.com>
2023-03-08 09:18:30 +0200

    vaadin v bump

--- src/main/java/com/vaadin/starter/skeleton/AppShell.java
+++ src/main/java/com/vaadin/starter/skeleton/AppShell.java
@@ -1,11 +1,10 @@
 package com.vaadin.starter.skeleton;
 
-import com.vaadin.flow.component.dependency.CssImport;
 import com.vaadin.flow.component.page.AppShellConfigurator;
 import com.vaadin.flow.server.PWA;
+import com.vaadin.flow.theme.Theme;
 
 @PWA(name = "Project Base for Vaadin", shortName = "Project Base")
-@CssImport("./styles/shared-styles.css")
-@CssImport(value = "./styles/vaadin-text-field-styles.css", themeFor = "vaadin-text-field")
+@Theme("my-theme")
 public class AppShell implements AppShellConfigurator {
 }
