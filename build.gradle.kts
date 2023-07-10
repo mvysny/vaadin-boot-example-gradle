c21c34b32234a8dbc7de1069c56be500d08dd6c5
Martin Vysny <mavi@vaadin.com>
2023-07-10 09:19:28 +0200

    vaadin v bump

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,7 +3,7 @@ import org.gradle.api.tasks.testing.logging.TestExceptionFormat
 plugins {
     java
     application
-    id("com.vaadin") version "24.1.0"
+    id("com.vaadin") version "24.1.2"
 }
 
 defaultTasks("clean", "build")
@@ -14,7 +14,7 @@ repositories {
 
 dependencies {
     // Vaadin
-    implementation("com.vaadin:vaadin-core:24.1.0") {
+    implementation("com.vaadin:vaadin-core:24.1.2") {
         afterEvaluate {
             if (vaadin.productionMode) {
                 exclude(module = "vaadin-dev")
