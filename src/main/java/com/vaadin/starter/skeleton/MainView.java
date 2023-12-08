6027cee5e3998b275cc71efc6b99e2085384197b
Martin Vysny <mavi@vaadin.com>
2023-12-08 10:37:15 +0200

    vaadin v bump

--- src/main/java/com/vaadin/starter/skeleton/MainView.java
+++ src/main/java/com/vaadin/starter/skeleton/MainView.java
@@ -7,6 +7,7 @@ import com.vaadin.flow.component.notification.Notification;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.component.textfield.TextField;
 import com.vaadin.flow.router.Route;
+import com.vaadin.flow.server.VaadinRequest;
 import com.vaadin.starter.skeleton.services.Services;
 
 /**
@@ -36,5 +37,7 @@ public class MainView extends VerticalLayout {
         addClassName("centered-content");
 
         add(textField, button);
+
+        System.out.println(VaadinRequest.getCurrent());
     }
 }
