ec93341eb8f0b81dd4fdd21fd621ceea8e18a300
Martin Vysny <mavi@vaadin.com>
2023-12-08 10:39:54 +0200

    revert accidental commit

--- src/main/java/com/vaadin/starter/skeleton/MainView.java
+++ src/main/java/com/vaadin/starter/skeleton/MainView.java
@@ -7,7 +7,6 @@ import com.vaadin.flow.component.notification.Notification;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.component.textfield.TextField;
 import com.vaadin.flow.router.Route;
-import com.vaadin.flow.server.VaadinRequest;
 import com.vaadin.starter.skeleton.services.Services;
 
 /**
@@ -37,7 +36,5 @@ public class MainView extends VerticalLayout {
         addClassName("centered-content");
 
         add(textField, button);
-
-        System.out.println(VaadinRequest.getCurrent());
     }
 }
