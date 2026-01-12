1267cd37381c235b38079e83f537649e72e522b7
Martin Vysny <mavi@vaadin.com>
2026-01-12 08:31:04 +0200

    refactor theming to Vaadin 25

--- src/main/java/com/vaadin/starter/skeleton/AppShell.java
+++ src/main/java/com/vaadin/starter/skeleton/AppShell.java
@@ -1,10 +1,12 @@
 package com.vaadin.starter.skeleton;
 
+import com.vaadin.flow.component.dependency.StyleSheet;
 import com.vaadin.flow.component.page.AppShellConfigurator;
 import com.vaadin.flow.server.PWA;
-import com.vaadin.flow.theme.Theme;
+import com.vaadin.flow.theme.lumo.Lumo;
 
 @PWA(name = "Project Base for Vaadin", shortName = "Project Base")
-@Theme("my-theme")
+@StyleSheet(Lumo.STYLESHEET)
+@StyleSheet("styles.css")
 public class AppShell implements AppShellConfigurator {
 }
